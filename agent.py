a7c4f0ec858189f7ad3ac55e87412fa09333c784
luisteixeira13 <lfctdeath@gmail.com>
2023-11-29 22:27:37 -0300

    Correções

--- agent.py
+++ agent.py
@@ -1,28 +1,23 @@
 # agent.py
+
 import random
+
 
 class Agent:
     # Lista de ações possíveis que o agente pode executar
-    ACTIONS = ['UP', 'DOWN', 'LEFT', 'RIGHT', 'SUCK', 'IDLE']
+    ACTIONS = ['UP', 'DOWN', 'LEFT', 'RIGHT', 'SUCK']
 
     def __init__(self, zone):
         # Inicializa o agente com uma referência ao ambiente (zone)
         self.zone = zone
 
-    def random_action(self):
-        # Gera aleatoriamente e retorna uma ação da lista de ações possíveis
-        return random.choice(self.ACTIONS)
-
     def perspective(self):
-        def perspective(self):
         # Atualiza a perspectiva do agente com base nas condições do ambiente
-            #self.bump = self.zone.is_bump()
-            #self.dirty = self.zone.is_dirty()
-            #self.positionX, self.positionY = self.zone.positionX, self.zone.positionY
-            pass
+        pass
 
-    def think(self):
-        # Método que representa a lógica de tomada de decisão do agente.
-        # Atualmente, retorna uma ação aleatória, mas pode ser expandido
-        # para uma lógica mais inteligente com base nas condições do ambiente.
-        return self.random_action()
+    def think(self, has_dirt):
+        if has_dirt:
+            self.zone.suck()  # Limpa se houver sujeira
+
+        # Em seguida, gera aleatoriamente uma nova ação
+        return random.choice(['UP', 'DOWN', 'LEFT', 'RIGHT'])
